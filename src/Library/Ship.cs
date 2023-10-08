2e2d8a06c00744bd16c603c44c0c42f6269568d8
Stefano <stefano.francolino@correo.ucu.edu.uy>
2023-10-08 12:42:35 -0300

    ClassShip

---
+++ src/Library/Ship.cs
@@ -0,0 +1,12 @@
+namespace BattleShip
+{
+    public class Ship
+    {
+        public string Name { get; set; }
+
+        public Ship(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
