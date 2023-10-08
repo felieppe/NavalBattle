06d4515bcf0bfad24a8bcf30935b97c52b9874c5
Lucía Amor <141635491+lu-amor@users.noreply.github.com>
2023-10-08 17:50:26 -0300

    Board & BoardSize documentation

--- src/Library/Board.cs
+++ src/Library/Board.cs
@@ -5,23 +5,40 @@ using System.Threading.Tasks;
 
 namespace BattleShip
 {
+    /// <summary>
+    /// Clase Board.
+    /// </summary>
     public class Board
     {
         private char[][] boards;
         private BoardSize boardSize;
 
+/// <summary>
+/// Constructor de la clase Board.
+/// </summary>
+/// <param name="boards"></param>
+/// <param name="boardSize"></param>
         public Board(char[][] boards, BoardSize boardSize)
         {
             this.boards = boards;
             this.boardSize = boardSize;
         }
 
+/// <summary>
+/// Set de la clase Board.
+/// </summary>
+/// <param name="boards"></param>
+/// <param name="boardSize"></param>
         public void SetBoard(char[][] boards, BoardSize boardSize)
         {
             this.boards = boards;
             this.boardSize = boardSize;
         }
 
+/// <summary>
+/// Get de la clase Board.
+/// </summary>
+/// <returns></returns>
         public char[][] GetBoard()
         {
             return this.boards;
--- src/Library/BoardSize.cs
+++ src/Library/BoardSize.cs
@@ -5,12 +5,26 @@ using System.Threading.Tasks;
 
 namespace BattleShip
 {
+    /// <summary>
+    /// Clase para establecer las dimensiones del tablero.
+    /// </summary>
     public class BoardSize
     {
+        /// <summary>
+        /// Filas del tablero.
+        /// </summary> <summary>
+        /// Columnas del tablero.
+        /// </summary>
+        /// <value></value>
         public int Rows { get; private set; }
 
         public int Columns { get; private set; }
 
+/// <summary>
+/// Dimensiones del tablero.
+/// </summary>
+/// <param name="rows"></param>
+/// <param name="columns"></param>
         public BoardSize(int rows, int columns)
         {
             this.Rows = rows;
