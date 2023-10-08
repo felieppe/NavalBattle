2a5be1136d2bf2693bedd5a6a26324c2dbc9c508
Lucía Amor <141635491+lu-amor@users.noreply.github.com>
2023-10-08 18:59:46 -0300

    board con filas y columnas

--- src/Library/Board.cs
+++ src/Library/Board.cs
@@ -13,15 +13,50 @@ namespace BattleShip
         private char[][] boards;
         private BoardSize boardSize;
 
-/// <summary>
-/// Constructor de la clase Board.
-/// </summary>
-/// <param name="boards"></param>
-/// <param name="boardSize"></param>
+        /// <summary>
+        /// Constructor de la clase Board.
+        /// </summary>
+        /// <param name="boards"></param>
+        /// <param name="boardSize"></param>
         public Board(char[][] boards, BoardSize boardSize)
         {
             this.boards = boards;
             this.boardSize = boardSize;
+            this.InitializeBoard();
+        }
+
+        /// <summary>
+        /// Inicializar la matriz del tablero con etiquetas de fila y columna.
+        /// </summary>
+        public void InitializeBoard()
+        {
+            int rows = this.boardSize.Rows;
+            int columns = this.boardSize.Columns;
+
+            this.boards = new char[rows + 1][];
+            for (int i = 0; i <= rows; i++)
+            {
+                this.boards[i] = new char[columns + 1];
+                for (int j = 0; j <= columns; j++)
+                {
+                    if (i == 0 && j == 0)
+                    {
+                        this.boards[i][j] = ' ';
+                    }
+                    else if (i == 0)
+                    {
+                        this.boards[i][j] = (char)('A' + j - 1);
+                    }
+                    else if (j == 0)
+                    {
+                        this.boards[i][j] = (char)('0' + i);
+                    }
+                    else
+                    {
+                        this.boards[i][j] = ' ';
+                    }
+                }
+            }
         }
 
 /// <summary>
@@ -41,7 +76,7 @@ namespace BattleShip
 /// <returns></returns>
         public char[][] GetBoard()
         {
-            return this.boards;
+            return this.boards; // Devuelve el tablero.
         }
     }
 }
