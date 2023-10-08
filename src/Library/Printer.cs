3abff010a7527c802753797be7778f3808161205
Lucia Vazquez <luciavazquezreyes299@gmail.com>
2023-10-08 20:51:47 -0300

    Printer

---
+++ src/Library/Printer.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BattleShip;
+
+namespace Library{
+    public class Printer
+{
+    private Board board;
+    private int Width;
+    private int Height;
+
+    public Printer(Board board, int width, int height)
+    {
+        this.board = board;
+        this.Width = width;
+        this.Height = height;
+    }
+
+    public void Print()
+    {
+        while (true)
+        {
+            Console.Clear();
+
+            StringBuilder s = new StringBuilder();
+            for (int y = 0; y < Height; y++)
+            {
+                for (int x = 0; x < Width; x++)
+                {
+                    if (board.GetCell(x, y))
+                    {
+                        s.Append("|X|");
+                    }
+                    else
+                    {
+                        s.Append("___");
+                    }
+                }
+                s.Append("\n");
+            }
+        }
+    }
+}
+
+}
---
+++ test/LibraryTests/Printertest.cs
@@ -0,0 +1,20 @@
+using Library;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Tests
+{
+    /// <summary>
+    /// Prueba de la clase <see cref="Game"/>.
+    /// </summary>
+    [TestFixture]
+    public class PrinterTests{
+
+    
+    public void TestPrint(){
+        
+
+    }
+
+    }
+}
