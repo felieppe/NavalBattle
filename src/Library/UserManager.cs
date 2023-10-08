65cbf1cbce5d3418999b5a4c39c42e9f753959d9
Felipe <me@felieppe.com>
2023-10-08 14:22:01 -0300

    NewGame() func added

--- src/Library/UserManager.cs
+++ src/Library/UserManager.cs
@@ -19,6 +19,37 @@ namespace Library
             this.Players.Remove(player);
         }
 
+        private void AddInGamePlayers(Game game) {
+            foreach (Player p in game.GetPlayers()) {
+                InGamePlayers.Add(p);
+            }
+        }
+
+        public Game NewGame() {
+            if (!(this.Players.Count > 1)) { return null; }
+
+            List<Player> AvailablePlayers = Players.Except(InGamePlayers).ToList();
+            if (!(AvailablePlayers.Count > 1)) { return null; }
+
+            Random rnd = new Random();
+            int pIndex = 0, pIndex2 = 0;
+            bool ready = false;
+            while (!ready) {
+                pIndex = rnd.Next(AvailablePlayers.Count);
+                pIndex2 = rnd.Next(AvailablePlayers.Count);
+
+                if (pIndex != pIndex2) { ready = true; }
+            }
+
+            Player player1 = AvailablePlayers[pIndex];
+            Player player2 = AvailablePlayers[pIndex2];
+            Game game = new Game(player1, player2);
+
+            AddInGamePlayers(game);
+
+            return game;
+        }
+
         public List<Player> GetPlayers() {
             return this.Players;
         }
