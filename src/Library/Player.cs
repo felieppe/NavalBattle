7ab26fd1f512b8e772a8ddabb70cb5a619757f6d
Felipe <me@felieppe.com>
2023-10-08 15:46:44 -0300

    Implemented username as parameter for Player

--- src/Library/Player.cs
+++ src/Library/Player.cs
@@ -8,6 +8,8 @@ namespace Library
     public class Player
     {
         private string Id {get; set;}
+        private string Username {get; set;}
+        
         public Player(string id) {
             this.Id = id;
         }
@@ -15,9 +17,15 @@ namespace Library
         public void SetId(string id) {
             if (!string.IsNullOrEmpty(id)) { this.Id = id; }
         }
+        public void SetUsername(string username) {
+            if (!string.IsNullOrEmpty(username)) { this.Username = username; }
+        }
 
         public string GetId() {
             return this.Id;
+        }
+        public string GetUsername() {
+            return this.Username;
         }
     }
 }
