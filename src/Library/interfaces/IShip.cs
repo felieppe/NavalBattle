17d822627f0225f756820d55c1cebe20c93bc3a0
Lucia Vazquez <luciavazquezreyes299@gmail.com>
2023-10-09 18:25:38 -0300

    ,

--- src/Library/interfaces/IShip.cs
+++ src/Library/interfaces/IShip.cs
@@ -1,7 +1,7 @@
 namespace Library {
     public interface IShip {
         string Name {get; set;}
-        int Lenght {get; set;}
+        int Length {get; set;}
 
         bool Sunken {get; set;}
         bool GetSunken();
