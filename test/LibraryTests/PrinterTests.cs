3d8f0575c6eb3dd5a2b674859027228fb9b1a1d8
Felipe Cabrera <me@felieppe.com>
2023-10-10 10:44:54 -0300

    Printer testcase class now have zero tests

--- test/LibraryTests/PrinterTests.cs
+++ test/LibraryTests/PrinterTests.cs
@@ -9,42 +9,8 @@ using NUnit.Framework;
 namespace Tests
 {
     /// <summary>
-    /// Prueba de la clase <see cref="Game"/>.
+    /// Prueba de la clase <see cref="Printer"/>.
     /// </summary>
-/* [TestFixture]
-    public class PrinterTests
-    {
-        private Board board;
-        private char[][] board1;
-        private BoardSize bs;
-
-        [SetUp]
-        public void SetUp()
-        {
-            this.board = new Board(this.board1, this.bs);
-        }
-
-        [Test]
-        public void TestPrint()
-        {
-            int rows = 3;
-            int columns = 4;
-            char[][] expectedBoard = new char[4][]
-            {
-                new char[] { ' ', 'A', 'B', 'C', 'D' },
-                new char[] { '1', ' ', ' ', ' ', ' ' },
-                new char[] { '2', ' ', ' ', ' ', ' ' },
-                new char[] { '3', ' ', ' ', ' ', ' ' },
-            };
-            string expected = expectedBoard.ToString();
-            BoardSize boardSize = new BoardSize(rows, columns);
-            Board board = new Board(this.board1, boardSize);
-
-            board.InitializeBoard();
-            char[][] actualBoard = board.GetBoard();
-            string actual = actualBoard.ToString();
-
-            Assert.AreEqual(expected, actual);
-        }
-    }*/
+    [TestFixture]
+    public class PrinterTests {}
 }
