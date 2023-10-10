012e0a46a2ff26f2588c2ad4bd6d2b486a68064c
Stefano <stefano.francolino@correo.ucu.edu.uy>
2023-10-09 21:09:53 -0300

    IShip Implmentada Correctamente

--- src/Library/vessels/core/Ship.cs
+++ src/Library/vessels/core/Ship.cs
@@ -3,20 +3,24 @@ namespace Library
     public class Ship : IShip
     {
         public string Name { get; set; }
-        public int Lenght { get; set; }
+        public int Length { get; set; }  // Corregir la propiedad Length
         public bool Sunken { get; set; }
-        public Ship(string name, int lenght)
+
+        public Ship(string name, int length)  // Corregir el nombre del parámetro
         {
             this.Name = name;
-            this.Lenght = lenght;
+            this.Length = length;  // Corregir la asignación de la propiedad Length
         }
 
-        public void SetSunken(bool s) {
+        public void SetSunken(bool s)
+        {
             this.Sunken = s;
         }
 
-        public bool GetSunken() {
+        public bool GetSunken()
+        {
             return this.Sunken;
         }
     }
 }
+
