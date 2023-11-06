8fc431ed201b4c010fbc27bc0433a126178bcfdb
Felipe <me@felieppe.com>
2023-11-06 14:18:20 -0300

    AddGame() func now checks if game is duplicated or not

--- src/Library/ServerManager.cs
+++ src/Library/ServerManager.cs
@@ -10,7 +10,17 @@ namespace Library
         private List<Game> Servers = new List<Game>();
 
         public void AddGame(Game game) {
-            if (game != null) {this.Servers.Add(game);}
+            if (game != null) {
+                bool duplicated = false;
+                foreach (Game server in this.Servers) {
+                    if (server.GetGameId() == game.GetGameId()) {
+                        duplicated = true;
+                        break;
+                    }
+                }
+
+                if (!duplicated) { this.Servers.Add(game); }
+            }
         }
         public void RemoveGame(string id) {
             Game g = this.Servers.FirstOrDefault(g => g.GetGameId() == id);
