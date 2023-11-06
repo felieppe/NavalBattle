513baffb9471d7bf7c3f5609ee153cb2de537f05
Felipe <me@felieppe.com>
2023-11-06 09:25:01 -0300

    Coords class created

---
+++ src/Library/Coords.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class Coords
+    {
+        private int X;
+        private int Y;
+
+        public Coords(int x, int y) {
+            this.X = x;
+            this.Y = y;
+        }
+    }
+}
