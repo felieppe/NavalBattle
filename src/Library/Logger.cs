88fca4c4d9046c2a041db7418306cd779435f70e
Felipe <me@felieppe.com>
2023-11-15 15:40:37 -0300

    Setup() func added and runned after class init

--- src/Library/Logger.cs
+++ src/Library/Logger.cs
@@ -1,4 +1,6 @@
+using System.Net;
 using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,8 +12,25 @@ namespace Library
     {
         private Configuration Config;
 
+        private void Setup() {
+            string folderPath = "../../logs";
+            if (!Directory.Exists(folderPath)) { Directory.CreateDirectory(folderPath); }
+
+            string GenerateLogfile() {
+                DateTime currentTime = DateTime.UtcNow;
+                int timestamp = (int)(currentTime.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+
+                return $"{timestamp}_@{this.Config.GetUsername()}.log";
+            }
+            string logFile = GenerateLogfile();
+            
+            string logPath = $"{folderPath}/{logFile}";
+            if (!File.Exists(logPath)) { File.Create(logPath); }
+        }
+
         public Logger(Configuration config) {
             this.Config = config;
+            this.Setup();
         }
 
         public void Info(string msg) {
