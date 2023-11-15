6d70c30a94024e6975afa7310c9d17c11d0626ea
Felipe <me@felieppe.com>
2023-11-15 14:52:59 -0300

    Using Library & config var added

--- src/Library/Logger.cs
+++ src/Library/Logger.cs
@@ -2,11 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Library;
 
 namespace Library
 {
     public class Logger
     {
+        private Configuration config;
+
         public Logger() {}
     }
 }
