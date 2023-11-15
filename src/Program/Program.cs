0b4c51046b384c0764f2c0c3e60bed8353258159
Felipe <me@felieppe.com>
2023-11-15 17:02:39 -0300

    Main bot code added

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,12 +1,13 @@
-﻿using System.Diagnostics;
-using System.Runtime.CompilerServices;
-using System.Globalization;
-//--------------------------------------------------------------------------------
+﻿//--------------------------------------------------------------------------------
 // <copyright file="Program.cs" company="Universidad Católica del Uruguay">
 //     Copyright (c) Programación II. Derechos reservados.
 // </copyright>
 //--------------------------------------------------------------------------------
 
+using System.Threading;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Globalization;
 using System;
 using Library;
 
@@ -17,12 +18,31 @@ namespace NavalBattle
     /// </summary>
     public class Program
     {
-        private static Configuration config = new Configuration();
-        private static Logger logger = new Logger(config);
+        private static Configuration Config = new Configuration();
+        private static Logger Logger = new Logger(config);
+        private static TelegramClientBot Bot;
 
         /// <summary>
         /// Punto de entrada al programa principal.
         /// </summary>
-        public static void Main() {}
+        public static void Main() {
+            Logger.Info($"Setting up @{Config.GetUsername()}...");
+
+            bot = new TelegramClientBot(Config.GetToken());
+            var cts = new CancellationTokenSource();
+
+            Bot.StartReceiving(
+                HandleUpdateAsync,
+                HandleErrorAsync,
+                new ReceiverOptions()
+                {
+                    AllowedUpdates = Array.Empty<UpdateType>()
+                },
+                cts.Token
+            );
+
+            Logger.Info($"{Config.GetUsername()} is up!");
+            Console.ReadLine();
+        }
     }
 }
