e7d28d861bbbcec016f9d8771a1d0c6469474200
Felipe <me@felieppe.com>
2023-11-15 13:40:49 -0300

    Removed all the test code

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -18,24 +18,7 @@ namespace NavalBattle
         /// Punto de entrada al programa principal.
         /// </summary>
         public static void Main() {
-            int rows = 20;
-            int columns = 20;
-            int totalShips = 5;
-            Game g = new Game(rows, columns, totalShips);
-
-            GameLogic gameLogic = new GameLogic(g, g.GetBoard1(), g.GetBoard2());
-
-            Battleship bship = new Battleship();
-            Cruise cruise = new Cruise();
-            gameLogic.PlaceShip(cruise, 'H', 5, "right");
-            gameLogic.PlaceShip(bship, 'F', 3, "down");
-
-            gameLogic.Attack('J', 5);
-
-            Board glBoard = gameLogic.GetBoard();
-            Console.WriteLine(glBoard);
-            Printer pr = new Printer();
-            pr.Print(glBoard);
+            //
         }
     }
 }
