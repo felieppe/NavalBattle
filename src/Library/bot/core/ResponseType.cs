91c5cc97690f56485b1bf38dc677dc420d748e1b
Felipe <me@felieppe.com>
2023-11-15 17:49:55 -0300

    ResponseType class created

---
+++ src/Library/bot/core/ResponseType.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.bot.core
+{
+    public enum ResponseType {
+        Message, Image, Video, Sticker, Keyboard
+    }
+}
