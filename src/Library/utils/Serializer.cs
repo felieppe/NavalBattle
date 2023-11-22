d4bba0afa70560a71ce46b4628013c7764d7de6c
Felipe <me@felieppe.com>
2023-11-21 23:26:30 -0300

    Serialize() func added

--- src/Library/utils/Serializer.cs
+++ src/Library/utils/Serializer.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Library.utils.core;
 
 namespace Library.utils
 {
@@ -17,5 +18,17 @@ namespace Library.utils
         }
 
         public Serializer() {}
+
+        #nullable enable
+        public void Serialize(DataType opt, Game? game, Player? player) {
+            switch (opt) {
+            case DataType.Game:
+                if (game != null) { return; }
+                break;
+            case DataType.Player:
+                if (player != null) { return; }
+                break;
+            }
+        }
     }
 }
