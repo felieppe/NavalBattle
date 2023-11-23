05ceb1c183ae78a1081607aecc08caaa43ce04e6
feli <35511790+felieppe@users.noreply.github.com>
2023-11-23 16:19:31 -0300

    Deserialization method for game saves implemented

--- src/Library/utils/Deserializer.cs
+++ src/Library/utils/Deserializer.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Library.utils.core;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Library.utils
 {
@@ -24,7 +27,51 @@ namespace Library.utils
 
             switch (opt) {
                 case DataType.Game:
-                    break;
+                    string serversFolder = $"{baseFolder}/servers/";
+                    string[] files = Directory.GetFiles(serversFolder, "*.json");
+
+                    List<Game> games = new List<Game>();
+
+                    foreach (var file in files) {
+                        string json = File.ReadAllText(file);
+                        JObject obj = JObject.Parse(json);
+
+                        // Retriving game data from JSON object
+                        string id = obj["id"].ToString();
+                        List<Coords> shipsCoords = JsonConvert.DeserializeObject<List<Coords>>(obj["ships_coords"].ToString());
+                        List<Ship> ships = JsonConvert.DeserializeObject<List<Ship>>(obj["ships"].ToString());
+                        int totalShips = obj["total_ships"].Value<int>();
+                        List<Player> players = JsonConvert.DeserializeObject<List<Player>>(obj["players"].ToString());
+                        Player admin = JsonConvert.DeserializeObject<Player>(obj["admin"].ToString());
+                        int rows = obj["rows"].Value<int>();
+                        int columns = obj["columns"].Value<int>();
+                        Board board1 = JsonConvert.DeserializeObject<Board>(obj["board_1"].ToString());
+                        Board board2 = JsonConvert.DeserializeObject<Board>(obj["board_2"].ToString());
+
+                        // Cloning the game data to a new instance
+                        Game game = new Game(rows, columns, totalShips);
+                        
+                        game.SetGameId(id);
+                        game.SetAdmin(admin);
+                        game.SetBoard1(board1);
+                        game.SetBoard2(board2);
+
+                        foreach (Coords coord in shipsCoords) {
+                            game.AddShipCoords(coord.GetShipId(), coord.GetX(), coord.GetY());
+                        }
+                        foreach (Ship ship in ships) {
+                            game.AddShip(ship);
+                        }
+                        foreach (Player player in players) {
+                            game.AddPlayer(player);
+                        }
+
+                        // Adding game to the returnable games list
+                        games.Add(game);
+                    }
+
+                    Logger.Instance.Info($"Has been retrieved {games.Count} games!");
+                    return games;
                 case DataType.Player:
                     break;
             }
