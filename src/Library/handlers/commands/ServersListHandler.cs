fb5182af973cc299d264c448e259051c965e6d60
Felipe Cabrera <me@felieppe.com>
2023-11-26 17:49:22 -0300

    Every server will now display the session name in servers list

---
+++ src/Library/handlers/commands/ServersListHandler.cs
@@ -0,0 +1,67 @@
+
+using System.Collections.Generic;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
+using Library.handlers.core;
+using Library.bot;
+using Library.bot.core;
+
+namespace Library.handlers
+{
+    /// <summary>
+    /// Un "handler" del patrón Chain of Responsibility que implementa los comandos "servers" y "join".
+    /// </summary>
+    public class ServersListHandler : BaseHandler
+    {
+        /// <summary>
+        /// Inicializa una nueva instancia de la clase <see cref="PlayHandler"/>.
+        /// </summary>
+        /// <param name="next"> El próximo "handler". </param>
+        public ServersListHandler(BaseHandler next) : base(next)
+        {
+            this.Keywords = new string[] { "/servers", "/join" };
+        }
+
+        /// <summary>
+        /// Procesa el mensaje "servers" y retorna true; retorna false en caso contrario.
+        /// </summary>
+        /// <param name="message"> El mensaje a procesar. </param>
+        /// <param name="response"> La respuesta al mensaje procesado. </param>
+        /// <returns> true si el mensaje fue procesado; false en caso contrario. </returns>
+        protected override void InternalHandle(Message message, out Response response)
+        {
+            User author = message.From;
+            string answr = $"This are the available servers to play! If you want to create one, go back.";
+
+            List<Game> availableServers = ServerManager.Instance.GetListing();
+            List<InlineKeyboardButton[]> buttons = new List<InlineKeyboardButton[]>();
+            
+            int x = 1;
+            foreach (Game server in availableServers)
+            {
+                buttons.Add(new []
+                {
+                    InlineKeyboardButton.WithCallbackData(text: $"{x}. {server.GetSessionName()} ({server.GetPlayers().Count}/2)", callbackData: $"show_server-{server.GetGameId()}")
+                });
+                x += 1;
+            }
+
+            if (x == 1)
+            {
+                buttons.Add(new []
+                {
+                    InlineKeyboardButton.WithCallbackData(text: $"No servers available! 🚮", callbackData: $"none")
+                });
+            }
+
+            buttons.Add(new []
+            {
+                InlineKeyboardButton.WithCallbackData(text: $"Return 🔙", callbackData: $"return-/menu")
+            });
+            InlineKeyboardMarkup inlineKeyboard = buttons.ToArray();
+
+            response = new Response(ResponseType.Keyboard, answr);
+            response.SetKeyboard(inlineKeyboard);
+        }
+    }
+}
