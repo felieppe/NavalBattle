4813cdca136b1ab11e3c6b93ff6b843584f6d44e
Felipe Cabrera <me@felieppe.com>
2023-11-26 17:15:11 -0300

    Return handler added

---
+++ src/Library/handlers/callbacks/ReturnHandler.cs
@@ -0,0 +1,37 @@
+
+using System.Collections.Generic;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
+using Library.handlers.core;
+using Library.bot;
+using Library.bot.core;
+
+namespace Library.handlers
+{
+    /// <summary>
+    /// Un "handler" del patrón Chain of Responsibility que implementa los comandos "servers" y "join".
+    /// </summary>
+    public class ReturnHandler : BaseHandler
+    {
+        /// <summary>
+        /// Inicializa una nueva instancia de la clase <see cref="PlayHandler"/>.
+        /// </summary>
+        /// <param name="next"> El próximo "handler". </param>
+        public ReturnHandler(BaseHandler next) : base(next)
+        {
+            this.Keywords = new string[] { "return" };
+        }
+
+        /// <summary>
+        /// Procesa el mensaje "servers" y retorna true; retorna false en caso contrario.
+        /// </summary>
+        /// <param name="message"> El mensaje a procesar. </param>
+        /// <param name="response"> La respuesta al mensaje procesado. </param>
+        /// <returns> true si el mensaje fue procesado; false en caso contrario. </returns>
+        protected override void InternalHandle(Message message, out Response response)
+        {
+            string whereReturn = message.Text.Split("-")[1];
+            response = new Response(ResponseType.Return, "", ret: whereReturn);
+        }
+    }
+}
