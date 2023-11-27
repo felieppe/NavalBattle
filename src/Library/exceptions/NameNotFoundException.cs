d174c72a5b09d5cd973226540b21b9aa7c79e74f
Stefano <stefano.francolino@correo.ucu.edu.uy>
2023-11-27 10:05:24 -0300

    Excepciones

--- src/Library/exceptions/NameNotFoundException.cs
+++ src/Library/exceptions/NameNotFoundException.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 
 namespace Library.Exceptions
-{
+{   /// <summary>
+    /// Excepcion por si no se encuentra el nombre 
+    /// </summary>
     public class NameNotFoundException : Exception
     {
         public NameNotFoundException() : base() {}
--- src/Library/exceptions/UsernameNotFoundException.cs
+++ src/Library/exceptions/UsernameNotFoundException.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 
 namespace Library.Exceptions
-{
+{   /// <summary>
+    /// Excepción por si no se encuentra el nombre de usuario  
+    /// </summary>
     public class UsernameNotFoundException : Exception
     {
         public UsernameNotFoundException() : base() {}
